7ba36e9f56d256c8a421f9ba1ca5b118aa65d2b3
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-07-25 17:20:44 +0900

    initial commit to learn Java. primitive data types

---
+++ src/first_program/Application.java
@@ -0,0 +1,43 @@
+package first_program;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		//Number variables in java
+		//Java has quite a variety of number variables.
+		
+		//byte are small numbers (1 byte size) from -128 to 127
+		byte myByte = 1;
+		
+		//short are 2 bytes size numbers from  -32,768 to 32,767
+		short myShort = 10000;
+		
+		//int are 4 bytes size numbers from -2,147,483,648 to 2,147,483,647
+		int myInt = 1234567890;
+		
+		//long are bigger numbers, 8 bytes size from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		//dont forget to add the L at the end
+		long myLong = 12345678912345L;
+		
+		//float store fractional numbers until 6 or 7 digits
+		//dont forget to add the f at the end
+		float myFloat = 2.123456f;
+		
+		//double stores longer fractional numbers until 15 digits
+		double myDouble = 6.1234567890;
+		
+		//
+		
+		//the console.log() in Java
+		System.out.println(myByte);
+		System.out.println(myShort);
+		System.out.println(myInt);
+		System.out.println(myLong);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		
+		
+		 
+	}
+	}
