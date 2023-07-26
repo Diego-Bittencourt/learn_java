f65da8e773577ceb5d34f96ef049758f8a2dfbe3
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-07-26 17:24:23 +0900

    while loop and for loop
    the syntax is the same as is in javascript

---
+++ src/first_program/Application3.java
@@ -0,0 +1,27 @@
+package first_program;
+
+public class Application3 {
+  
+
+	public static void main(String[] args) {
+
+		double value = 0;
+		
+		//while loop works like in javascript
+		while(value < 10)
+		{
+			
+			System.out.println(value);
+			value = value + 0.1;
+		}
+		
+		//for loops receives three parts, just like javascript
+		//the first runs before the loop, the second is the conditional and the last part runs after each iteration
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
+
+
+
