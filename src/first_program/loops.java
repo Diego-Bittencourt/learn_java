155943a195521229864834db0e79e21fd211fd14
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-07-27 18:25:38 +0900

    do while loops

--- src/first_program/loops.java
+++ src/first_program/loops.java
@@ -1,5 +1,6 @@
 package first_program;
 
+import java.util.Scanner;
 public class loops {
   
 
@@ -20,6 +21,17 @@ public class loops {
 		for(int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
+
+		Scanner scanner = new Scanner(System.in);
+		int uservalue = 0;
+
+		do {
+			System.out.println("Enter a number");
+			uservalue = scanner.nextInt();
+		} 
+		while (uservalue != 5); 
+			System.out.println("Got 5!");
+		
 	}
 }
 
